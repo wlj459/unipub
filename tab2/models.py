5016bd2ce626868fcaa2bf16837c9785ea43f4d6
libai <libai@mogujie.com>
2015-08-10 20:32:42 +0800

    先建个表

--- tab2/models.py
+++ tab2/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from usr.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(u'类型', max_length=50)
+
+    class Meta:
+        verbose_name = u'类型'
+        verbose_name_plural = u'类型类'
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
+
+class Article(models.Model):
+    title = models.CharField(u'标题名', max_length=200)
+    author = models.ForeignKey(User, u'作者名', max_length=200)
+    category = models.ForeignKey(Category, verbose_name=u'类型')
+    is_send = models.BooleanField(u'是否显示', default=False)
+    published = models.DateTimeField(u'发布时间', auto_now_add=True)
+    content = models.TextField(u'正文')
+    clicks = models.IntegerField(u'点击次数', default=0)
+
+    class Meta:
+        ordering = ['-published']
+        verbose_name = u'文章'
+        verbose_name_plural = u'文章类'
