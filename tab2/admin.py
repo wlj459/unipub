a8c2e9438e3a4b057f32c04b3c8957bc848cc970
libai <libai@mogujie.com>
2015-08-11 21:21:39 +0800

    admin tinymce

--- tab2/admin.py
+++ tab2/admin.py
@@ -1,2 +1,18 @@
+#-*- coding:utf-8 -*-
 from django.contrib import admin
 
+from models import *
+
+
+class ArticleAdmin(admin.ModelAdmin):#文章类admin后台显示以及tinymce
+    list_display = ('title', 'show', 'time')
+    fields = ('title', 'show', 'content')
+
+    class Media:
+        js = (
+        '/static/js/tinymce/tinymce.min.js',
+        '/static/js/tinymce/config.js',
+        )
+
+
+admin.site.register(Article, ArticleAdmin)
